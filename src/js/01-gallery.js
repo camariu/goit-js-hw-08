f18116c85b89c0f081bbddf99a888aee020d60c1
camariu <amariutei1998@gmail.com>
2023-06-21 12:34:58 +0300

    hw-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+ 
+import { galleryItems } from "./gallery-items.js";
+// import "simplelightbox/dist/simple-lightbox.min.css"
+// import simpleLightbox from "simplelightbox";
 
-console.log(galleryItems);
+const ulItemGalery = document.querySelector(".gallery");
+
+function galleryCreate() {
+  const galleryItem = galleryItems
+    .map(
+      ({ preview, description, original }) =>
+        ` <li class = "gallery__item">
+  <a class="gallery__link" href="${original}">
+      <img class = "gallery__image"
+      src = "${preview}"
+      alt = "${description}">
+</a></li>`
+    )
+    .join("");
+
+  ulItemGalery.insertAdjacentHTML("beforeend", galleryItem);
+
+    }
+
+    galleryCreate()
+
+// var lightbox = new SimpleLightbox('.gallery a', {captionsData:'alt' , captionDelay:250});
